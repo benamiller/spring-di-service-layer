9ec565d40101d4177c770548bfecb8503b083cb1
Hactually Benji <mrbenamiller@gmail.com>
2023-03-01 18:22:19 +0000

    Add JUnit test structure

---
+++ src/test/java/com/hactually/ServiceTest.java
@@ -0,0 +1,26 @@
+package com.hactually;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ServiceTest {
+    @BeforeClass
+    public static void init() {
+        // create instance of service with dao and view
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void displayOrderInformation() {
+        // assertEquals(expectedOrder, actualOrder)
+        assertEquals(1, 2);
+    }
+
+    @Test
+    public void generateOrderNumber() {
+        // Look through test folder of orders to get correct order number
+        assertEquals(1, 1);
+    }
+}
