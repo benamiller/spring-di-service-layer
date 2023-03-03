7b73031afd73ae539bfee2839455d04c2728c466
Hactually Benji <mrbenamiller@gmail.com>
2023-03-03 18:01:20 +0000

    Add method to get taxRates by state from file

--- src/main/java/com/hactually/dao/FlooringOrders.java
+++ src/main/java/com/hactually/dao/FlooringOrders.java
@@ -1,12 +1,13 @@
 package com.hactually.dao;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.*;
 
 public class FlooringOrders implements Orders {
-
+    private Map<String, Integer> taxRatesByState = new HashMap<>();
+    private Map<String, Double[]> productCostAndLabourCostPerSquareFoot = new HashMap<>();
 
     // Add the order to the file
     public void addOrder() {
@@ -14,19 +15,35 @@ public class FlooringOrders implements Orders {
     }
 
     // Retrieves taxRates from file
-    public Map<String, Integer> getTaxRates() {
+    public Map<String, Integer> getTaxRates(String filename) {
+        Scanner sc;
+        try {
+            sc = new Scanner(
+                    new BufferedReader(
+                            new FileReader(filename)
+                    ));
+            sc.nextLine();
+            while(sc.hasNextLine()) {
+                String stateInfo = sc.nextLine();
+                String[] stateInfoUnmarshalled = stateInfo.split(",");
+                String stateInitialism = stateInfoUnmarshalled[0];
+                int stateTaxRate = Integer.parseInt(stateInfoUnmarshalled[2]);
+                taxRatesByState.put(stateInitialism, stateTaxRate);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Was not able to find the Tax file");
+            throw new RuntimeException(e);
+        }
 
-        Map<String, Integer> map = new HashMap<>();
-        map.put("TX", 1);
-
-        return map;
+        return taxRatesByState;
     }
 
     public Map<String, Double[]> getProductTypeCosts() {
         Map<String, Double[]> map = new HashMap<>();
         Double[] arr = {2.25, 2.10};
         map.put("Carpet", arr);
+        productCostAndLabourCostPerSquareFoot.put("Carpet", arr);
 
-        return map;
+        return productCostAndLabourCostPerSquareFoot;
     }
 }
